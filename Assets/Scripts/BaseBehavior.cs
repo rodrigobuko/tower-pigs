6d8f8f18705e3a95ce30291a7a2490d161182785
rodrigobuko <rodrigomalhaesbuko@gmail.com>
2021-10-17 08:16:39 -0300

    Scene adjustments

--- Assets/Scripts/BaseBehavior.cs
+++ Assets/Scripts/BaseBehavior.cs
@@ -1,11 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaseBehavior : MonoBehaviour {
     public PlayerInventory playerInventory;
+    public int life;
+    public GameObject LifeText;
+
+    private void Start() {
+        LifeText = GameObject.FindWithTag("TextMoney");
+        LifeText.GetComponent<Text>().text = life.ToString();
+    }
+
     public void TakeDamage(int damage) {
-        playerInventory.life -= damage;
+        Debug.Log("OBA");
+        life -= damage;
+        LifeText.GetComponent<Text>().text = life.ToString();
     }
 }
